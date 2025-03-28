4fc953a86187659d32c1ef3dfd8c6c41607568e0
Pranjal Sinha <pranjal01sinha@gmail.com>
2025-03-28 10:23:37 +0530

    added home page

--- app/src/main/java/com/example/payease/HomeActivity.kt
+++ app/src/main/java/com/example/payease/HomeActivity.kt
@@ -2,7 +2,9 @@ package com.example.payease
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ArrayAdapter
+import android.widget.ImageButton
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.payease.auth.FirebaseAuthManager
@@ -14,7 +16,11 @@ class HomeActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
         val sessionManager = SessionManager(this)
-        val logoutButton = findViewById<Button>(R.id.logout)
+        val logoutButton = findViewById<ImageButton>(R.id.logout)
+        val listView= findViewById<ListView>(R.id.listView)
+        val employeeNames = listOf("John Doe", "Jane Smith", "Michael Johnson", "Emily Davis", "Daniel Brown")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, employeeNames)
+        listView.adapter = adapter
 
         logoutButton.setOnClickListener {
             FirebaseAuthManager.logout()
